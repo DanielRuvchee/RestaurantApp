dab0ecd0a21322be1d60c0e1cb47bbdf4dd4604e
Daniel <daniel.ruvcheski@students.finki.ukim.mk>
2024-10-14 23:58:05 +0200

    Supabase conf and sign-up  page

---
+++ src/lib/supabase.ts
@@ -0,0 +1,27 @@
+import 'react-native-url-polyfill/auto';
+import * as SecureStore from 'expo-secure-store';
+import { createClient } from '@supabase/supabase-js';
+
+const ExpoSecureStoreAdapter = {
+  getItem: (key: string) => {
+    return SecureStore.getItemAsync(key);
+  },
+  setItem: (key: string, value: string) => {
+    SecureStore.setItemAsync(key, value);
+  },
+  removeItem: (key: string) => {
+    SecureStore.deleteItemAsync(key);
+  },
+};
+
+const supabaseUrl = 'https://dynoahpkmeyfaortjuam.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR5bm9haHBrbWV5ZmFvcnRqdWFtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjg5NDEzMjIsImV4cCI6MjA0NDUxNzMyMn0.OlTi84Q7Wjo-h5OB6lNpYhp6nHIn1CNLDXpPybads-I';
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  auth: {
+    storage: ExpoSecureStoreAdapter as any,
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: false,
+  },
+});
