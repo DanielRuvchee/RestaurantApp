49e2b21366f916aaaf2f7dc9a91e4046998abcfd
Daniel <daniel.ruvcheski@students.finki.ukim.mk>
2024-10-16 19:12:38 +0200

    Supabase typescript

--- src/components/ProductListItem.tsx
+++ src/components/ProductListItem.tsx
@@ -3,14 +3,14 @@ import { StyleSheet , Image, Pressable} from 'react-native';
 import { Text, View,  } from '@components/Themed';
 
 import Colors from '@/src/constants/Colors';
-import { Product } from '../types';
+import {  Tables } from '../types';
 import { Link, useSegments } from 'expo-router';
 
 export const defaultPizzaImage = 
 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/default.png';
 
 type ProductListItemProps = {
-    product: Product;
+    product: Tables<'products'>;
 }
 
 
--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,7 @@
 import 'react-native-url-polyfill/auto';
 import * as SecureStore from 'expo-secure-store';
 import { createClient } from '@supabase/supabase-js';
+import { Database } from '../database.types';
 
 const ExpoSecureStoreAdapter = {
   getItem: (key: string) => {
@@ -17,7 +18,7 @@ const ExpoSecureStoreAdapter = {
 const supabaseUrl = 'https://dynoahpkmeyfaortjuam.supabase.co';
 const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR5bm9haHBrbWV5ZmFvcnRqdWFtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjg5NDEzMjIsImV4cCI6MjA0NDUxNzMyMn0.OlTi84Q7Wjo-h5OB6lNpYhp6nHIn1CNLDXpPybads-I';
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+export const supabase = createClient<Database>(supabaseUrl, supabaseAnonKey, {
   auth: {
     storage: ExpoSecureStoreAdapter as any,
     autoRefreshToken: true,
